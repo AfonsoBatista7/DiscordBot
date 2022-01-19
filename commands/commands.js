2be30b985f9a0e5de9d43b736b62ba58e3dee6de
Afonso Batista <afonsobatista13@gmail.com>
2022-01-19 21:52:19 +0000

    Clean code

--- commands/commands.js
+++ commands/commands.js
@@ -7,7 +7,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setColor('#DF2700')
         .addFields(
-            {name: 'Commands', value: '\`.pila\` - Conta verdades\n\`.youtube\` - Dá print do canal do RageCraft :D\n\`.server\` - Mostra info do server se este estiver aberto\n\`.start\` - Liga o server se estiver fechado\n\`.balance <user>\` - Diz o dinheiro que o user tem\n\`.profile <user>\` - Perfil do user\n\`.mine\` - Minera até encontrar minérios para ganhar dinheiro\n\`.coin <heads/tails> <value>\` - Coin flip\n\`.give <user> <value>\` - give <value> money to an user.'}
+            {name: 'Commands', value: '\`.pila\` - Conta verdades\n\`.youtube\` - Dá print do canal do RageCraft :D\n\`.stats <playerName>\` - Mostra os status de um player num server de Minecraft\n\`.server\` - Mostra info do server se este estiver aberto\n\`.start\` - Liga o server se estiver fechado\n\`.balance <user>\` - Diz o dinheiro que o user tem\n\`.profile <user>\` - Perfil do user\n\`.mine\` - Minera até encontrar minérios para ganhar dinheiro\n\`.coin <heads/tails> <value>\` - Coin flip\n\`.give <user> <value>\` - give <value> money to an user.'}
         )
 
         message.channel.send({embeds: [embed]});
